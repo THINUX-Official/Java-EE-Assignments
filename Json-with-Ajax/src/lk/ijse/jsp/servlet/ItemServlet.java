6f74d5cbea75f2a8ccd451b47b2aa57695a51cd5
THINUX-Official <thinuxofficial@gmail.com>
2023-09-03 22:50:53 +0530

    Create doPost method in ItemServlet.

--- Json-with-Ajax/src/lk/ijse/jsp/servlet/ItemServlet.java
+++ Json-with-Ajax/src/lk/ijse/jsp/servlet/ItemServlet.java
@@ -56,52 +56,52 @@ public class ItemServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         String code = req.getParameter("code");
-        String itemName = req.getParameter("description");
-        String qty = req.getParameter("qty");
+        String name = req.getParameter("description");
+        String qtyOnHand = req.getParameter("qty");
         String unitPrice = req.getParameter("unitPrice");
+
         String option = req.getParameter("option");
 //
         try {
             Class.forName("com.mysql.jdbc.Driver");
+
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos_one", "root", "1234");
+
             switch (option) {
                 case "add":
                     PreparedStatement pstm = connection.prepareStatement("insert into Item values(?,?,?,?)");
+
                     pstm.setObject(1, code);
-                    pstm.setObject(2, itemName);
-                    pstm.setObject(3, qty);
+                    pstm.setObject(2, name);
+                    pstm.setObject(3, qtyOnHand);
                     pstm.setObject(4, unitPrice);
+
                     if (pstm.executeUpdate() > 0) {
-                        resp.getWriter().println("Item Added..!");
-                        resp.sendRedirect("item");
+                        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                        objectBuilder.add("message", "Item Added!");
+                        resp.setContentType("application/json");
+                        resp.getWriter().print(objectBuilder.build());
+                    } else {
+                        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                        objectBuilder.add("message", "Item Addition Failed!");
+                        resp.setContentType("application/json");
+                        resp.setStatus(400);
+                        resp.getWriter().print(objectBuilder.build());
                     }
+
                     break;
-                case "delete":
-                    PreparedStatement pstm2 = connection.prepareStatement("delete from Item where code=?");
-                    pstm2.setObject(1, code);
-                    if (pstm2.executeUpdate() > 0) {
-                        resp.getWriter().println("Item Deleted..!");
-                        resp.sendRedirect("/jsonp/pages/item.html");
-                    }
-                    break;
-                case "update":
-                    PreparedStatement pstm3 = connection.prepareStatement("update Item set description=?,qtyOnHand=?,unitPrice=? where code=?");
-                    pstm3.setObject(1, itemName);
-                    pstm3.setObject(2, qty);
-                    pstm3.setObject(3, unitPrice);
-                    pstm3.setObject(4, code);
-                    if (pstm3.executeUpdate() > 0) {
-                        resp.getWriter().println("Item Updated..!");
-                        resp.sendRedirect("/jsonp/pages/item.html");
-                    }
-                    break;
+
             }
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+            objectBuilder.add("error", e.getMessage());
+            resp.setContentType("application/json");
+            resp.setStatus(400);
+            resp.getWriter().print(objectBuilder.build());
         }
     }
 }
